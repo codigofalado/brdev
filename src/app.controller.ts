efcafec2d6be293fb41b5b77f5bcd42b219ff943
Fernando Santos <fern@ndo.io>
2021-01-26 23:27:05 -0300

    Configurando Variáveis de Ambiente (fecha #2 )

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private configService: ConfigService) {}
 
   @Get()
   getHello(): string {
@@ -12,5 +13,13 @@ export class AppController {
   @Get('brdev')
   getBrDev(): string {
     return this.appService.getBrDev();
+  }
+  @Get('environment')
+  getEnvironment(): string {
+    const dbpass = this.configService.get<string>('DATABASE_PASSWORD');
+    const dbuser = this.configService.get<string>('DATABASE_USER');
+    return `Database User: ${dbuser} - <br>
+    Database Pass: ${dbpass}
+    `;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
