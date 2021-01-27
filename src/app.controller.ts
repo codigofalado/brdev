dd45c801b052294d162c1cc4ba36c2cf1f609af1
Fernando Santos <fern@ndo.io>
2021-01-26 22:50:29 -0300

    Testando o Teste com um Action no Controller
    ... e um método a mais no serviço App.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('brdev')
+  getBrDev(): string {
+    return this.appService.getBrDev();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    return 'Hello World!';
+  }
+  getBrDev(): string {
     return 'Hello BR.DEV! ⌨️🐒';
   }
 }
