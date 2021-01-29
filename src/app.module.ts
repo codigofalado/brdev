e6bde4215011245e0f61d6d01512264d92203ada
Fernando Santos <fern@ndo.io>
2021-01-28 22:20:13 -0300

    Novo Módulo de Teste: Deletar (ref #6)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { DeletarModule } from './deletar/deletar.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
-  })],
+  }), DeletarModule],
   controllers: [AppController],
   providers: [AppService],
 })
